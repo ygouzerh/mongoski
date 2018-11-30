5cac07b6cb5099ca7bd5813d9d78038b2b459a0e
Yohan Gouzerh <yohan.gouzerh@grenoble-inp.org>
2018-11-30 21:43:40 +0100

    Add query one and two
    Add some string treatement on the first.

---
+++ querie_one.js
@@ -0,0 +1,22 @@
+// Queries
+
+// Récupérer les noms et le massif des stations, ainsi que l'altitude
+// sous la chaine: "'Massif' - 'Nom' ('altitudeMin'-'altitudeMax' m): X pistes noires." (sans les quotes) avec plus de 5 pistes noires
+db.station.aggregate([
+  {$unwind: "$pistes"},
+  {$match : {"pistes.niveau": "noir"}},
+  {$group:
+    {_id:"$_id",
+    "nom":{$first: "$nom"},
+    "massif":{$first: "$massif"},
+    "altitudeMin":{$first: "$altitudeMin"},
+    "altitudeMax":{$first: "$altitudeMax"},
+    "count": {$sum:1}}},
+  {$match : {"count": {$gte: 5}}},
+  {$project: {"station":
+    {$concat:
+      ["$massif", " - ", "$nom", " (", {$toString: "$altitudeMin"}, "-", {$toString: "$altitudeMax"}, " m): ",
+      {$toString: "$count"} ," pistes noires."]
+    }
+  }}
+]).pretty();
