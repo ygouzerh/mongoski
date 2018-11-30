ec385814e3b254ed5301246eabfbd6a078698628
Yohan Gouzerh <yohan.gouzerh@grenoble-inp.org>
2018-11-30 21:43:40 +0100

    Add query one and two
    Add some string treatement on the first.

--- querie_two.js
+++ querie_two.js
@@ -1,18 +1,24 @@
-// Queries
-
-// Récupérer les pisteurs qui s'occupent des remontées mécaniques
+// Récupérer les noms et les prénoms des pisteurs qui s'occupent des remontées mécaniques
 db.station.aggregate([
-  {$project: {"pisteurs":1, "remonteesMecaniques.employe_id":1}},
-  {$unwind: "$pisteurs"},
-  {$unwind: "$remonteesMecaniques"},
-  // {
-  //   $redact: {
-  //     $cond:{
-  //       if: {$eq: ["pisteurs", "5bfec1f8e3a97b8830ea9f2d"]},
-  //       then: "$$KEEP",
-  //       else: "$$PRUNE"
-  //     }
-  //   }
-  // }
-  {$where: function(){return (this.pisteurs == "5bfec1f8e3a97b8830ea9f2d")}}
-]).pretty();
+    // Explode the arrays
+    {$unwind: "$pisteurs"},
+    {$unwind: "$remonteesMecaniques"},
+    // Verify that our keys exists
+    {$match:{"pisteurs":{"$exists":true},"remonteesMecaniques.employe_id":{"$exists":true}}},
+    // Compare the two fields and filter
+    {$project: {
+        "pisteurs":1,
+        "Cmp": {"$cmp":["$pisteurs","$remonteesMecaniques.employe_id"]}
+    }
+    },
+    // Check equals
+    {$match:{"Cmp":0}},
+    // Get the employer
+    {$lookup: {
+        from: "employe",
+        localField: "pisteurs",
+        foreignField: "_id",
+        as: "employe",
+    }},
+    {$project: {"employe.prenom":1, "employe.nom":1}}
+])
--- queries.js
+++
@@ -1,9 +0,0 @@
-// Queries
-
-// Récupérer les stations avec plus de 5 pistes noires
-db.station.aggregate([
-  {$unwind: "$pistes"},
-  {$match : {"pistes.niveau": "noir"}},
-  {$group: {_id:"$_id", "nom":{$first: "$nom"}, "count": {$sum:1}}},
-  {$match : {"count": {$gte: 5}}}
-]);
